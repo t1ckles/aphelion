9859c1e4db2d8779af06f66c5ab20bb64948b13a
t1ckles <bob.risdon@gmail.com>
2026-04-19 18:46:41 -0400

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1126,7 +1126,9 @@ function cmdDock() {
 
   const loc     = playerState.location;
   const q       = galaxy.quadrants[loc.quadrantIndex];
-  const cluster = q.clusters.find(c => c.name === loc.clusterName);
+  const cluster = loc.clusterName
+    ? q.clusters.find(c => c.name === loc.clusterName)
+    : q.clusters[loc.clusterIndex || 0];
   const sys     = cluster && cluster.systems.find(s => s.name === loc.systemName);
   if (!sys) return '  [ERROR] Location data corrupted.';
 
